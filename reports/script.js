06e9bbd101acb0e28afbfb077200a2128cf84eb4
kayere <brainhesley3@gmail.com>
2019-11-05 19:06:29 +0300

    added charts

--- reports/script.js
+++ reports/script.js
@@ -1,3 +1,74 @@
 $(document).ready(function(){
-    
+    incomeChart();
+    expenditureChart();
 });
+
+function incomeChart(){
+    //bar
+var ctxB = $('#income');
+var myBarChart = new Chart(ctxB, {
+type: 'bar',
+data: {
+labels: ["Salary", "Debts", "Business"],
+datasets: [{
+label: 'Income sources',
+data: [5000, 20000, 60000],
+backgroundColor: [
+'rgba(255, 99, 132, 0.2)',
+'rgba(54, 162, 235, 0.2)',
+'rgba(75, 192, 192, 0.2)'
+],
+borderColor: [
+'rgba(255,99,132,1)',
+'rgba(54, 162, 235, 1)',
+'rgba(75, 192, 192, 1)'
+],
+borderWidth: 1
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero: true
+}
+}]
+}
+}
+});
+}
+
+function expenditureChart(){
+    //bar
+var ctxB = $('#expenditure');
+var myBarChart = new Chart(ctxB, {
+type: 'bar',
+data: {
+labels: ["School fees", "Loans", "Food"],
+datasets: [{
+label: 'Where spent',
+data: [70000, 10000, 40000],
+backgroundColor: [
+'rgba(255, 206, 86, 0.2)',
+'rgba(75, 192, 192, 0.2)',
+'rgba(255, 159, 64, 0.2)'
+],
+borderColor: [
+'rgba(255, 206, 86, 1)',
+'rgba(75, 192, 192, 1)',
+'rgba(255, 159, 64, 1)'
+],
+borderWidth: 1
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero: true
+}
+}]
+}
+}
+});
+}
