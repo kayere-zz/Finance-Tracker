53382aa33f0d1d885100bd661b696abe5ccc1834
kayere <brainhesley3@gmail.com>
2019-11-07 00:35:21 +0300

    changed report values

--- reports/script.js
+++ reports/script.js
@@ -132,21 +132,21 @@ function previous_expenditureChart(){
                 backgroundColor: 'rgba(255, 206, 86, 0.2)',
                 borderColor: 'rgba(255, 206, 86, 1)',
                 borderWidth: 1, 
-                data: [3,7,4]
+                data: [50000,70000,30000]
             },
             {
                 label: "Loans",
                 backgroundColor: 'rgba(255, 206, 86, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1,
-                data: [4,3,5]
+                data: [60000,30000,50000]
             },
             {
                 label: "Food",
                 backgroundColor: 'rgba(255, 159, 64, 0.2)',
                 borderColor: 'rgba(255, 159, 64, 1)',
                 borderWidth: 1,
-                data: [7,2,6]
+                data: [70000,25000,30000]
             }
         ]
     };
