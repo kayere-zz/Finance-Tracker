76b3b2238d711e420e91f3501c31e0e3dbd882aa
kayere <brainhesley3@gmail.com>
2019-11-07 00:31:54 +0300

    added previous months reports

--- reports/script.js
+++ reports/script.js
@@ -52,7 +52,7 @@ label: '',
 data: [70000, 10000, 40000],
 backgroundColor: [
 'rgba(255, 206, 86, 0.2)',
-'rgba(75, 192, 192, 0.2)',
+'rgba(255, 206, 86, 0.2)',
 'rgba(255, 159, 64, 0.2)'
 ],
 borderColor: [
@@ -75,10 +75,94 @@ beginAtZero: true
 });
 }
 
-function previous_income(){
+function previous_incomeChart(){
+    var ctx = $('#previous_income');
 
+var data = {
+    labels: ["August", "September", "October"],
+    datasets: [
+        {
+            label: "Salary",
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor:'rgba(255,99,132,1)',
+            borderWidth: 1,
+            data: [30000,70000,40000]
+        },
+        {
+            label: "Debts",
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor:'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+            data: [40000,20000,50000]
+        },
+        {
+            label: "Business",
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor:'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            data: [70000,20000,60000]
+        }
+    ]
+};
+
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        barValueSpacing: 20,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                }
+            }]
+        }
+    }
+});
 }
 
 function previous_expenditureChart(){
+    var ctx = $('#previous_expenditure');
+
+    var data = {
+        labels: ["August", "September", "October"],
+        datasets: [
+            {
+                label: "School fees",
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                borderColor: 'rgba(255, 206, 86, 1)',
+                borderWidth: 1, 
+                data: [3,7,4]
+            },
+            {
+                label: "Loans",
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+                data: [4,3,5]
+            },
+            {
+                label: "Food",
+                backgroundColor: 'rgba(255, 159, 64, 0.2)',
+                borderColor: 'rgba(255, 159, 64, 1)',
+                borderWidth: 1,
+                data: [7,2,6]
+            }
+        ]
+    };
     
+    var myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {
+            barValueSpacing: 20,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                    }
+                }]
+            }
+        }
+    });
 }
